f8a16a7f01a61aed2ca34482d9296e6882bc158d
phixyn <phixyn@gmail.com>
2017-10-01 19:33:42 +0100

    feat(main): add code to spawn entities in the level scene

--- GameAIDemos/src/Main.cpp
+++ GameAIDemos/src/Main.cpp
@@ -12,8 +12,20 @@ int main()
 	// TODO: Temporary, asset doesn't exist but arg is required
 	const std::string BACKGROUND_IMAGE = "assets/background.png";
 
+	// TODO temporary placeholder entities for testing
+	GameAIDemos::LiveEntity tempEntity = GameAIDemos::LiveEntity(100, sf::Vector2f(100.f, 100.f), sf::Vector2f(10.f, 10.f));
+	tempEntity.setColor(sf::Color(sf::Color::Red));
+	GameAIDemos::LiveEntity tempEntity2 = GameAIDemos::LiveEntity(100, sf::Vector2f(50.f, 50.f), sf::Vector2f(150.f, 150.f));
+
+	// Create game and level scene instances
 	GameAIDemos::Game game(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
-	game.addScene(std::make_unique<GameAIDemos::LevelScene>(game, BACKGROUND_IMAGE));
+	GameAIDemos::LevelScene testScene = GameAIDemos::LevelScene(game, BACKGROUND_IMAGE);
+	// Spawn entities in level scene
+	testScene.spawnLiveEntity(tempEntity);
+	testScene.spawnLiveEntity(tempEntity2);
+	testScene.spawnLiveEntity(100, sf::Vector2f(50.f, 50.f), sf::Vector2f(250.f, 250.f));
+	// Add scene to game and start main loop
+	game.addScene(std::make_unique<GameAIDemos::LevelScene>(testScene));
 	game.mainLoop();
 
 	return 0;
