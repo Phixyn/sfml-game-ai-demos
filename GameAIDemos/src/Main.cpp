950e67359b3804e06c9de7f3caef919fac3ed5ee
phixyn <phixyn@gmail.com>
2017-10-01 01:19:19 +0100

    feat(main): call game.addScene in main()

--- GameAIDemos/src/Main.cpp
+++ GameAIDemos/src/Main.cpp
@@ -1,4 +1,5 @@
 #include "../include/Game.hpp"
+#include "../include/LevelScene.hpp"
 
 /// <summary>
 /// Entry point for the game.
@@ -8,8 +9,11 @@ int main()
 	const unsigned int WINDOW_WIDTH = 800;
 	const unsigned int WINDOW_HEIGHT = 600;
 	const std::string WINDOW_TITLE = "SFML Game AI Demos";
+	// TODO: Temporary, asset doesn't exist but arg is required
+	const std::string BACKGROUND_IMAGE = "assets/background.png";
 
 	GameAIDemos::Game game(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+	game.addScene(std::make_unique<GameAIDemos::LevelScene>(game, BACKGROUND_IMAGE));
 	game.mainLoop();
 
 	return 0;
