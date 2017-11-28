9eab278213179e92f3adf7725133a6c27e8b485f
Alpeche Pancha <phixyn@gmail.com>
2017-11-28 15:02:28 +0000

    feat(AttackState): add usage of a reference to a LiveEntity object for a target entity to attack

--- GameAIDemos/include/AttackEntityState.hpp
+++ GameAIDemos/include/AttackEntityState.hpp
@@ -2,6 +2,7 @@
 #define ATTACK_ENTITY_STATE_H
 
 #include "BaseEntityState.hpp"
+#include "LiveEntity.hpp"
 
 namespace GameAIDemos
 {
@@ -11,43 +12,57 @@ namespace GameAIDemos
 	/// </summary>
 	class AttackEntityState : public BaseEntityState
 	{
-	public:
-		/// <summary>
-		/// Default constructor. Calls the super class constructor, passing
-		/// in the name of this state.
-		/// </summary>
-		AttackEntityState();
-		/// <summary>
-		/// Default destructor.
-		/// </summary>
-		~AttackEntityState() { }
+		public:
+			/// <summary>
+			/// Default constructor. Calls the super class constructor, passing
+			/// in the name of this state.
+			/// </summary>
+			AttackEntityState();
+			/// <summary>
+			/// Default constructor. Calls the super class constructor, passing
+			/// in the name of this state.
+			/// </summary>
+			///
+			/// <param> TODO </param>
+			AttackEntityState(LiveEntity* target);
+			/// <summary>
+			/// Default destructor.
+			/// </summary>
+			~AttackEntityState() { }
 
-		/// <summary>
-		/// Implementation of the enter() virtual method for the attacking
-		/// state. Performs any initialization actions necessary upon
-		/// entering this state.
-		/// </summary>
-		void enter();
-		/// <summary>
-		/// Implementation of the update() virtual method for the attacking
-		/// state. Carries out the attacking state actions.
-		/// </summary>
-		void update();
-		/// <summary>
-		/// Implementation of the enter() virtual method for the attacking
-		/// state. Performs any clean up actions necessary upon exiting
-		/// this state.
-		/// </summary>
-		void exit();
+			/// <summary>
+			/// Implementation of the enter() virtual method for the attacking
+			/// state. Performs any initialization actions necessary upon
+			/// entering this state.
+			/// </summary>
+			///
+			/// <param> TODO </param>
+			void enter();
+			/// <summary>
+			/// Implementation of the update() virtual method for the attacking
+			/// state. Carries out the attacking state actions.
+			/// </summary>
+			void update();
+			/// <summary>
+			/// Implementation of the enter() virtual method for the attacking
+			/// state. Performs any clean up actions necessary upon exiting
+			/// this state.
+			/// </summary>
+			void exit();
 
-		/// <summary>
-		/// Returns a pointer to a new state.
-		/// </summary>
-		///
-		/// <returns>
-		/// A pointer to a new state.
-		/// </returns>
-		BaseEntityState* getNewState();
+			/// <summary>
+			/// Returns a pointer to a new state.
+			/// </summary>
+			///
+			/// <returns>
+			/// A pointer to a new state.
+			/// </returns>
+			BaseEntityState* getNewState();
+
+			/// <summary>
+			/// TODO (+ make private)
+			/// </summary>
+			LiveEntity* m_target;
 	};
 }
 
--- GameAIDemos/src/AttackEntityState.cpp
+++ GameAIDemos/src/AttackEntityState.cpp
@@ -1,7 +1,13 @@
 #include "../include/AttackEntityState.hpp"
+#include "../include/IdleEntityState.hpp"
 
 GameAIDemos::AttackEntityState::AttackEntityState() : BaseEntityState("Attacking")
 {
+}
+
+GameAIDemos::AttackEntityState::AttackEntityState(LiveEntity* target) : AttackEntityState()
+{
+	m_target = target;
 }
 
 void GameAIDemos::AttackEntityState::enter()
@@ -10,13 +16,21 @@ void GameAIDemos::AttackEntityState::enter()
 
 void GameAIDemos::AttackEntityState::update()
 {
+	// m_target->setHealth(m_target->getHealth() - 1);
+	// std::cout << m_target->getHealth() << std::endl;
+	if (m_target->isDead())
+	{
+		exit();
+	}
 }
 
 void GameAIDemos::AttackEntityState::exit()
 {
+	// TODO
+	// delete m_target;
 }
 
 GameAIDemos::BaseEntityState* GameAIDemos::AttackEntityState::getNewState()
 {
-	return nullptr;
+	return new IdleEntityState();
 }
