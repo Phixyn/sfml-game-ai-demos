b8a2882d1a7423d7a98890e44e8be40b74657b9b
Alpeche Pancha <phixyn@gmail.com>
2017-11-28 15:03:32 +0000

    style(State): fix indentation

--- GameAIDemos/include/DefendEntityState.hpp
+++ GameAIDemos/include/DefendEntityState.hpp
@@ -11,43 +11,43 @@ namespace GameAIDemos
 	/// </summary>
 	class DefendEntityState : public BaseEntityState
 	{
-	public:
-		/// <summary>
-		/// Default constructor. Calls the super class constructor, passing
-		/// in the name of this state.
-		/// </summary>
-		DefendEntityState();
-		/// <summary>
-		/// Default destructor.
-		/// </summary>
-		~DefendEntityState() { }
+		public:
+			/// <summary>
+			/// Default constructor. Calls the super class constructor, passing
+			/// in the name of this state.
+			/// </summary>
+			DefendEntityState();
+			/// <summary>
+			/// Default destructor.
+			/// </summary>
+			~DefendEntityState() { }
 
-		/// <summary>
-		/// Implementation of the enter() virtual method for the defending
-		/// state. Performs any initialization actions necessary upon
-		/// entering this state.
-		/// </summary>
-		void enter();
-		/// <summary>
-		/// Implementation of the update() virtual method for the defending
-		/// state. Carries out the defending state actions.
-		/// </summary>
-		void update();
-		/// <summary>
-		/// Implementation of the enter() virtual method for the defending
-		/// state. Performs any clean up actions necessary upon exiting
-		/// this state.
-		/// </summary>
-		void exit();
+			/// <summary>
+			/// Implementation of the enter() virtual method for the defending
+			/// state. Performs any initialization actions necessary upon
+			/// entering this state.
+			/// </summary>
+			void enter();
+			/// <summary>
+			/// Implementation of the update() virtual method for the defending
+			/// state. Carries out the defending state actions.
+			/// </summary>
+			void update();
+			/// <summary>
+			/// Implementation of the enter() virtual method for the defending
+			/// state. Performs any clean up actions necessary upon exiting
+			/// this state.
+			/// </summary>
+			void exit();
 
-		/// <summary>
-		/// Returns a pointer to a new state.
-		/// </summary>
-		///
-		/// <returns>
-		/// A pointer to a new state.
-		/// </returns>
-		BaseEntityState* getNewState();
+			/// <summary>
+			/// Returns a pointer to a new state.
+			/// </summary>
+			///
+			/// <returns>
+			/// A pointer to a new state.
+			/// </returns>
+			BaseEntityState* getNewState();
 	};
 }
 
